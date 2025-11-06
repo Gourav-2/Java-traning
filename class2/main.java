ab56ec261e307a7c835c667894077eeec1b663aa
Gourav-2 <gc27112006@gmail.com>
2025-11-06 10:54:46 +0530

    codes of java

---
+++ class2/main.java
@@ -0,0 +1,37 @@
+
+package class2;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        //*************************************imput lena*******************
+
+
+        // System.out.print("Enter integer");
+        // int a = sc.
+
+        // System.out.println("enter single word ");
+        // String b = sc.next();
+
+
+        // System.out.print("enter line ");
+        // String c =sc.next();
+
+       
+        // System.out.println("word"+b);
+        //  System.out.println("line" +c );
+
+
+        //************************ */ type casting *********************
+
+        // short a =130;
+        // int b=a;
+        // System.out.println(b);
+
+
+        
+       
+    }
+    
+}
---
+++ class2/tem.java
@@ -0,0 +1,34 @@
+package class2;
+import java.util.Scanner;
+public class tem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // System.out.println("enter single word ");
+        // String b = sc.next();
+    // int arr[]={1,2,3,4,5};
+    // for(int i =0;i<arr.length;i++){
+    //     System.out.println((arr[i]));
+
+    
+
+
+    // }
+    String str ="hello";
+    // for(int i =0;i<str.length();i++){
+    //     System.out.println(str.charAt(i));
+    // }
+
+    for(int i =str.length()-1;i>=0;i--){
+        System.out.println(str.charAt(i));
+    }
+
+
+
+        
+
+
+    }
+    
+
+}
