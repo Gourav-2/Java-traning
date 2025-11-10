334d0857fcb5d519afa9ff3ee3ba96278f08e233
Gourav-2 <gc27112006@gmail.com>
2025-11-10 22:08:58 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,33 @@
+package class2;
+
+public class string {
+    public static void main(String[] args) {
+        //*************************string*******
+        // String str ="hello";
+        // for(int i=0;i<str.length();i++){
+        //     System.out.println(str.charAt(i));
+
+        // }
+        
+        // String str ="hello";
+        // for(int i=str.length()-1;i>=0;i--){
+        //     System.out.println(str.charAt(i));
+
+        // }
+
+
+        //************************************pelendrom *************/
+        String str ="hello";
+        char[] ch = str.toCharArray();
+        for (int i =ch.length-1;i>=0;i--){
+            // System.out.print(ch[i]);
+            // short b = ch[i];
+            
+        }
+        
+        
+
+
+    }
+    
+}
