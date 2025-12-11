63dbfaa4d79646697d5856a2d0ed1c0ac0e67f4c
Gourav-2 <gc27112006@gmail.com>
2025-12-11 15:57:50 +0530

    thread

--- class14/multithreading.java
+++ class14/multithreading.java
@@ -2,18 +2,42 @@ package class14;
 class MyThread extends Thread{
     public void run(){
         System.out.println(Thread.currentThread().getName());
-        for(int i = 0;i<5;i++){
-            System.out.println(i+"t1");
+        for(int i = 1;i<=5;i++){
+            System.out.println("dowloading......"+(i*20)+"%");
+            try {
+                Thread.sleep(2000);//ye funtion time ke liaa thraed ko block kar deta hai
+            } catch (Exception e) {
+                System.out.println(e);
+                // TODO: handle exception
+            }
         }
     }
 }
 public class multithreading {
     public static void main(String[] args) {
-        MyThread T1=new MyThread();
-        T1.start();
-        for(int i =0;i<5;i++){
-            System.out.println(i+"main");
+        MyThread t1=new MyThread();
+        t1.start();
+
+        //  try {
+        //     t1.join();//join method se jab uppaa ka code nahi chlta jab tak neeche ka nahi chalta 
+        // } catch (Exception e) {
+        //     // TODO: handle exception
+        // }
+        
+        for(int i = 1;i<=5;i++){
+            System.out.println("timer"+i);
         }
+       
+        
+        
+        // for(int i =0;i<5;i++){
+        //     System.out.println(i+"main");
+        // }
+        
+        
+       
+        
+
         
     }
     
