29b4b55a88c77d28575fe9514ec04cb3f3db4ae5
Gourav-2 <gc27112006@gmail.com>
2025-12-14 22:48:31 +0530

    new

---
+++ class14/atm.java
@@ -0,0 +1,19 @@
+package class14;
+
+
+public class atm {
+    class bank{
+        int balance=100;
+        synchronized void withdraw(int amt){
+            System.out.println(Thread.currentThread().getName());
+            if (balance >=amt){
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
